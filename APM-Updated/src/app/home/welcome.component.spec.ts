af75698f519cf052e1ae8a497b545816381561e1
Benjamin Tse <benjamin.tse@leicabiosystems.com>
2017-10-14 00:08:42 +1100

    Add Jest

---
+++ APM-Updated/src/app/home/welcome.component.spec.ts
@@ -0,0 +1,9 @@
+describe('A working test', () => {
+    it('a passing test', () => {
+        expect(1).toBe(1);
+    });
+
+    it('another passing test', () => {
+        expect(2).toBe(2);
+    });
+});
