73eca4e72eeb9a00476cf25a1f0258bb55324afe
Benjamin Tse <benjamin.l.tse@gmail.com>
2017-11-24 18:51:37 +1100

    Add jest

--- APM-Updated/src/app/home/welcome.component.spec.ts
+++ APM-Updated/src/app/home/welcome.component.spec.ts
@@ -1,3 +1,7 @@
+import { TestBed } from '@angular/core/testing';
+import { AppModule } from '../app.module';
+import { WelcomComponent } from './welcome.component';
+
 describe('A working test', () => {
     it('a passing test', () => {
         expect(1).toBe(1);
@@ -5,5 +9,23 @@ describe('A working test', () => {
 
     it('another passing test', () => {
         expect(2).toBe(2);
+    });
+
+    it('and another test', () => {
+        expect('a').toBe('a');
+    });
+});
+
+describe('Template for a component test', () => {
+    let fixture;
+
+    beforeEach(done => {
+        const configure = (testBed: TestBed) => {
+            testBed.configureTestingModule({
+                imports: [ AppModule],
+            });
+        }
+
+        fixture = TestBed.createComponent(WelcomComponent);
     });
 });
